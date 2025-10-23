4b08161138b7554151ae0e1a00828bb11b3e908c
Bouvet Thomas <tabouvet@gmail.com>
2025-10-23 15:08:29 +0200

    add logs on manager page

--- app/manager-dashboard/layout.tsx
+++ app/manager-dashboard/layout.tsx
@@ -1,7 +1,7 @@
 import Logo from "@/components/ui/logo";
 import "../globals.css";
 import { SmallProfileCard } from "@/components/ui/profile-card";
-import { Separator } from "@radix-ui/react-select";
+import { Separator } from "@/components/ui/separator";
 import {HeaderTitle} from "@/components/ui/header-title";
 
 export default function ManagerLayout({
@@ -12,7 +12,7 @@ export default function ManagerLayout({
   // Root `app/layout.tsx` already renders <html> and <body> and sets fonts.
   // Nested layouts must not render html/body — use a wrapper element instead.
   return (
-    <div className="flex flex-col">
+    <div className="flex flex-col h-screen">
       <div className="flex flex-row items-center justify-between">
         <Logo />
         <HeaderTitle title="Manager Dashboard" className="absolute left-1/2 transform -translate-x-1/2" />
@@ -21,9 +21,8 @@ export default function ManagerLayout({
           lastName="User Last Name"
         />
       </div>
-        <Separator className="border-b" />
-
-      <main className="flex-1">{children}</main>
+      <Separator className="border-b p-1" />
+      <main className="flex-1 h-full">{children}</main>
     </div>
   );
 }
